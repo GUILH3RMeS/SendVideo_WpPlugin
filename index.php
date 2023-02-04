63d5243bbc81b7c9e87c1105d46424517f937878
GUILH3RMeS <tkzebrola@gmail.com>
2023-02-03 23:53:12 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: SendVideo
+ * Plugin URI: https://github.com/GUILH3RMeS/SendVideo_WpPlugin
+ * Description: Plugin feito para enviar videos para o banco de dados conforme pedido do cliente
+ * Version: 1.0
+ * Author: Saladdinha
+ * Author URI: https://github.com/GUILH3RMeS/
+ */
+
+ echo("First Test")
+?>
